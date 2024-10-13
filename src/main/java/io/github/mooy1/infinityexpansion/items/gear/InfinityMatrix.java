535bd7130228616e800d12c40bf8bf9388bd1eba
TGBUG <3770955706@qq.com>
2024-10-13 16:21:20 +0800

    Add files via upload

--- src/main/java/io/github/mooy1/infinityexpansion/items/gear/InfinityMatrix.java
+++ src/main/java/io/github/mooy1/infinityexpansion/items/gear/InfinityMatrix.java
@@ -1,13 +1,15 @@
 package io.github.mooy1.infinityexpansion.items.gear;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.Nonnull;
 
-import io.github.thebusybiscuit.slimefun4.utils.LoreBuilder;
+import io.github.thebusybiscuit.slimefun4.core.attributes.Rechargeable;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,9 +26,11 @@ import io.github.thebusybiscuit.slimefun4.core.attributes.Soulbound;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem;
 
-public final class InfinityMatrix extends SimpleSlimefunItem<ItemUseHandler> implements Listener, Soulbound, NotPlaceable, InfinityMatrixCharge, Rechargeable {
+
+public final class InfinityMatrix extends SimpleSlimefunItem<ItemUseHandler> implements Listener, Soulbound, NotPlaceable, Rechargeable {
 
     private final float capacity;
+    private final HashMap<Player, Location> lastLocations = new HashMap<>();
 
     public InfinityMatrix(ItemGroup category, SlimefunItemStack item, RecipeType type, ItemStack[] recipe, float capacity) {
         super(category, item, type, recipe);
@@ -44,30 +48,6 @@ public final class InfinityMatrix extends SimpleSlimefunItem<ItemUseHandler> imp
         p.setAllowFlight(true);
     }
 
-    @Override
-    public float getMaxItemCharge(ItemStack item) {
-        return capacity;
-    }
-
-    @Override
-    public float getItemCharge(ItemStack item) {
-        return InfinityMatrixCharge.super.getItemCharge(item);
-    }
-
-    @Override
-    public void setItemCharge(ItemStack item, float charge) {
-        InfinityMatrixCharge.super.setItemCharge(item, charge);
-    }
-
-    @Override
-    public boolean addItemCharge(ItemStack item, float charge) {
-        return Rechargeable.super.addItemCharge(item, charge);
-    }
-
-    @Override
-    public boolean removeItemCharge(ItemStack item, float charge) {
-        return Rechargeable.super.removeItemCharge(item, charge);
-    }
 
     @Nonnull
     @Override
@@ -84,6 +64,7 @@ public final class InfinityMatrix extends SimpleSlimefunItem<ItemUseHandler> imp
             List<String> lore = meta.getLore();
 
             Player p = e.getPlayer();
+            p.sendMessage(String.valueOf(item));
 
             Iterator<String> iterator = lore.listIterator();
 
@@ -127,37 +108,36 @@ public final class InfinityMatrix extends SimpleSlimefunItem<ItemUseHandler> imp
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
-        ItemStack item = p.getInventory().getItemInMainHand();
-        if (p.getAllowFlight()) {
-            float currentCharge = getItemCharge(item);
-            p.sendMessage(String.valueOf(currentCharge));
-            if (currentCharge > 0) {
-                setItemCharge(item, currentCharge - 1);
-                if (currentCharge < 101) {
-                    p.sendMessage(ChatColor.RED + "飞行器电量低!");
+        Location lastLocation = lastLocations.get(p);
+        Location currentLocation = p.getLocation();
+
+        if (lastLocation == null || lastLocation.distance(currentLocation) >= 1) {
+            lastLocations.put(p, currentLocation);
+
+            ItemStack item = (getItem());
+            p.sendMessage("test");
+            if (p.getAllowFlight()) {
+                p.sendMessage(String.valueOf(item));
+                p.sendMessage(String.valueOf(getItemCharge(item)));
+                p.sendMessage(String.valueOf(getMaxItemCharge(item)));
+                if (removeItemCharge(item, (float) lastLocation.distance(currentLocation))) {
+                    p.sendMessage(String.valueOf(getItemCharge(item)));
+                    if (getItemCharge(item) <= 128) {
+                        p.sendMessage(ChatColor.RED + "飞行器电量低!");
+                    }
+                } else {
+                    disableFlight(p);
+                    p.sendMessage(ChatColor.RED + "飞行器电量不足!");
                 }
-            } else {
-                disableFlight(p);
-                p.sendMessage(ChatColor.RED + "飞行器电量不足!");
             }
         }
-    getItemHandler2();
     }
 
 
-    @Nonnull
-    public ItemUseHandler getItemHandler2() {
-    return e -> {
-        ItemStack item = e.getItem();
-        ItemMeta meta = item.getItemMeta();
-        List<String> lore = null;
-        if (meta != null) {
-            lore = meta.getLore();
-        }
-        float currentCharge = getItemCharge(item);
-        if (lore != null) {
-            lore.add(ChatColor.GREEN + "电量: " + currentCharge + "/8192");
-        }
-    };
+    @Override
+    public float getMaxItemCharge(ItemStack item) {
+        return capacity;
     }
+
+
 }
